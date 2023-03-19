f8b3e4e99229601b3178994f7b8279e1ec92e85e
BlockJJam <blockjjam99@gmail.com>
2023-03-19 21:21:43 +0900

    feat: 인가처리까지 테스트

--- src/main/java/io/security/basicsecurity/config/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/config/SecurityConfig.java
@@ -7,60 +7,131 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 
 import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
+
+        return http.authorizeHttpRequests()
+                .requestMatchers("/user").hasRole("USER")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin().and().build();
+
+
+
+    }
+
+}
+
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http.authorizeHttpRequests((authorize) -> authorize
+//                        .requestMatchers("/admin/**").hasRole("ADMIN")
+//                        .anyRequest().permitAll())
+//                .httpBasic();
+//
+//        return http.build();
+//    }
+//
+//    @Bean
+//    public SecurityFilterChain filterChain2(HttpSecurity http) throws Exception{
+//        http.authorizeHttpRequests((authorize) -> authorize.anyRequest().permitAll())
+//                .formLogin();
+//
+//        return http.build();
+//    }
+
+
+/***** 섹션 1 ******/
 //    @Autowired
 //    private UserDetailsService userDetailsService;
 
-    @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user1 = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("1111")
-                .roles("USER")
-                .build();
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsService() {
+//        UserDetails user1 = User.withDefaultPasswordEncoder()
+//                .username("user")
+//                .password("1111")
+//                .roles("USER")
+//                .build();
+//
+//        UserDetails user2 = User.withDefaultPasswordEncoder()
+//                .username("admin")
+//                .password("1111")
+//                .roles("ADMIN")
+//                .build();
+//
+//        UserDetails user3 = User.withDefaultPasswordEncoder()
+//                .username("sys")
+//                .password("1111")
+//                .roles("SYS")
+//                .build();
+//        return new InMemoryUserDetailsManager(user1, user2, user3);
+//    }
 
-        UserDetails user2 = User.withDefaultPasswordEncoder()
-                .username("admin")
-                .password("1111")
-                .roles("ADMIN")
-                .build();
 
-        UserDetails user3 = User.withDefaultPasswordEncoder()
-                .username("sys")
-                .password("1111")
-                .roles("SYS")
-                .build();
-        return new InMemoryUserDetailsManager(user1, user2, user3);
-    }
-
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/user/**").hasRole("USER")
-                        .requestMatchers("/admin/pay").hasRole("ADMIN")
-                        .requestMatchers("/admin/**").access(new WebExpressionAuthorizationManager("hasRole('ADMIN') or hasRole('SYS')"))
-                        .anyRequest().authenticated());
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests(authorize -> authorize
+//                        .requestMatchers("/login").permitAll()
+//                        .requestMatchers("/user/**").hasRole("USER")
+//                        .requestMatchers("/admin/pay").hasRole("ADMIN")
+//                        .requestMatchers("/admin/**").access(new WebExpressionAuthorizationManager("hasRole('ADMIN') or hasRole('SYS')"))
+//                        .anyRequest().authenticated());
+//
+//        http.formLogin()
+//                .successHandler(new AuthenticationSuccessHandler() {
+//                    @Override
+//                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+//                        RequestCache requestCache = new HttpSessionRequestCache(); // 인증 성공하면, 원래 가려고 했던 캐싱정보를 꺼내서 활용
+//                        SavedRequest savedRequest = requestCache.getRequest(request, response);
+//                        String redirectUrl = savedRequest.getRedirectUrl();
+//                        response.sendRedirect(redirectUrl);
+//
+//                    }
+//                });
+//
+//        http.exceptionHandling()
+//                .authenticationEntryPoint(new AuthenticationEntryPoint() {
+//                    @Override
+//                    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+//                        response.sendRedirect("/login");
+//                    }
+//                })
+//                .accessDeniedHandler(new AccessDeniedHandler() {
+//                    @Override
+//                    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+//                        response.sendRedirect("/denied");
+//                    }
+//                });
 //        http.formLogin()
 //                .loginPage("/loginPage")
 //                .defaultSuccessUrl("/")
@@ -111,7 +182,7 @@ public class SecurityConfig {
 //        http.sessionManagement()
 //                .maximumSessions(1)
 //                .maxSessionsPreventsLogin(false);
-        http.formLogin();
-        return http.build();
-    }
-}
+//
+//        return http.build();
+//    }
+//}
--- src/main/java/io/security/basicsecurity/controller/SecurityController.java
+++ src/main/java/io/security/basicsecurity/controller/SecurityController.java
@@ -1,5 +1,11 @@
 package io.security.basicsecurity.controller;
 
+import jakarta.servlet.http.HttpSession;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,17 +18,56 @@ public class SecurityController {
     }
 
     @GetMapping("/user")
-    public String loginPage(){
+    public String user(){
         return "user";
     }
 
-    @GetMapping("/admin/pay")
-    public String adminPay(){
-        return "adminPay";
-    }
-
-    @GetMapping("admin/**")
-    public String admin(){
-        return "admin";
-    }
+//    @GetMapping("/")
+//    public String index(HttpSession session) {
+//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//        //session에서도 참조 가능
+//        SecurityContext context = (SecurityContext) session.getAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY);
+//        Authentication authentication2 = context.getAuthentication();
+//
+//        return "home";
+//    }
+//
+//    @GetMapping("/thread")
+//    public String thread(){
+//        new Thread(
+//                new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//
+//                    }
+//                }
+//        ).start();
+//        return "thread";
+//    }
+//
+//    @GetMapping("/user")
+//    public String loginPage() {
+//        return "user";
+//    }
+//
+//    @GetMapping("/admin/pay")
+//    public String adminPay() {
+//        return "adminPay";
+//    }
+//
+//    @GetMapping("admin/**")
+//    public String admin() {
+//        return "admin";
+//    }
+//
+//    @GetMapping("denied")
+//    public String denied(){
+//        return "Access is denied";
+//    }
+//
+//    @GetMapping("/login")
+//    public String login(){
+//        return "login";
+//    }
 }
